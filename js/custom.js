324611a1bf26eaa01a8dfdd3371ddc4c15e4a569
huangynj <huangynj@gmail.com>
2026-06-14 16:20:48 -0500

    Remove legacy Google Analytics and Custom Search Engine code

--- js/custom.js
+++ js/custom.js
@@ -16,13 +16,6 @@ function spiderjam(mym, myd){
     document.write("<a href=mailto:"	+ mym + "&#64;" + myd + ">" + mym + "&#64;" + myd + "</a>");
 }
 
-function firefox() {
-    var browser=navigator.userAgent.toLowerCase();
-    if(browser.indexOf('firefox') > -1) {
-        $('#bs-example-navbar-collapse-2').toggle();
-    }
-}
-
 function reset_menus() {
     sizing($(window).width());
 }
@@ -86,27 +79,6 @@ jQuery(document).ready(function($) {
                        internal_link();
                        $(window).on('hashchange', internal_link);
                        });
-
-
-// Google Site Search | Search box
-(function() {
- var cx = '012777421281883581498:r3xp4rmwevw';
- var gcse = document.createElement('script');
- gcse.type = 'text/javascript';
- gcse.async = true;
- gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
- '//cse.google.com/cse.js?cx=' + cx;
- var s = document.getElementsByTagName('script')[0];
- s.parentNode.insertBefore(gcse, s);
- })();
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-70658068-1', 'auto');
-ga('send', 'pageview');
 
 // Add reverse-tabnabbing protection to static and third-party injected links.
 (function() {
